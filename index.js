2f10a5350232b4351c96939e1870841196c62187
Seda Ünal <sedaylmz06@outlook.com>
2022-10-14 15:06:33 +0300

    test

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 39;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = { name, height, message };
 console.log(message);
+
+describe("what-is-a-test", () => {
+  describe("Susan", () => {
+    it('returns "Susan"', () => {
+      expect(name).toEqual("Susan");
+    });
+  });
+
+  describe("Height", () => {
+    it("is less than 40", () => {
+      expect(height).toBeLessThan(40);
+    });
+  });
+
+  describe("Message", () => {
+    it("gives the name and height", () => {
+      expect(message).toInclude(name);
+      expect(message).toInclude(height);
+    });
+  });
+});
